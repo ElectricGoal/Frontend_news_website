3318d224b63ddb04d33dbf024b4a873567c503ad
ElectricGoal <minhtu250802@gmail.com>
2022-12-26 15:50:57 +0700

    first commit

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container-fluid pb-2 border-bottom border-dark">
+        <div class="row px-4">
+            <div class="col">
+                <div class="row">
+                    <div class="fw-bold">{{ getDate }}</div>
+                </div>
+                <div class="row">
+                    <div>Tin tức hôm nay</div>
+                </div>
+            </div>
+            <a href="/" class="col-5 text-center">
+                <img src="../assets/logo_header.png" class="img-fluid" alt="logo">
+            </a>
+            <div class="col">
+                <div class="row">
+                    <div class="col-8">
+                        <form action="/search" method="get">
+                            <div class="input-group">
+                                <input type="text" name="q" class="form-control search-style" placeholder="Tìm kiếm">
+                                <button class="btn btn-outline-secondary" type="submit">
+                                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-1">
+                        <h4 class="my-1">
+                            <font-awesome-icon icon="fa-solid fa-circle-user" />
+                        </h4>
+                    </div>
+                    <div class="col my-auto">
+                        <a class="text-dark" href="/login">Đăng nhập</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script>
+
+export default {
+    name: 'HomeHeader',
+    computed: {
+        getDate() {
+            const date = new Date();
+
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
+
+            const weekday = ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"];
+            let dayOfWeek = weekday[date.getDay()];
+
+            let currentDate = `${dayOfWeek}, ${day}/${month}/${year}`
+            return currentDate
+        }
+    }
+}
+</script>
+    
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.search-style {
+    font-size: small !important;
+}
+</style>
---
+++ src/views/CategoryPage.vue
@@ -0,0 +1,91 @@
+<template>
+    <header>
+        <HomeHeader />
+    </header>
+
+    <nav>
+        <HomeNavBar :slug="category.slug" />
+    </nav>
+
+    <main>
+        <div class="container">
+            <div class="row input-style mt-4 border-bottom">
+                <div class="col">
+                    <h1 class="fw-bold display-4" style="font-family: Dosis">{{ category.name }}</h1>
+                </div>
+                <div class="col-4 position-relative">
+                    <button type="button" class="btn btn-primary position-absolute top-50 end-0 translate-middle-y">Nhận thông báo</button>
+                </div>
+            </div>
+
+            <div class="row mt-4 input-style">
+                <div v-if="!isFetching" class="col">
+                    <div v-for="item in this.news" :key="item.id">
+                        <NewCard :img="`http://127.0.0.1:8000${item.img}`" :title="item.title"
+                            :description="item.description" :category="item.category" :id="item.id"
+                            :date="item.date_posted" :isInSection="true"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <HomeFooter />
+    </footer>
+</template>
+  
+<script>
+import HomeHeader from '../components/HomeHeader.vue';
+import HomeNavBar from '../components/HomeNavBar.vue';
+import HomeFooter from '@/components/HomeFooter.vue';
+import NewCard from '@/components/NewCard.vue';
+import sourceData from '@/store/data_routes.json'
+import { HTTP } from '../api'
+
+export default {
+    name: 'CategoryPage',
+    components: {
+        HomeHeader,
+        HomeNavBar,
+        HomeFooter,
+        NewCard,
+    },
+    data() {
+        return {
+            news: [],
+            isFetching: true,
+        }
+    },
+    props: {
+        section: { type: String, required: true },
+    },
+    computed: {
+        category() {
+            // console.log(this.id)
+            return sourceData.categories.find(
+                (destination) => destination.slug === this.section
+            );
+        }
+    },
+    created() {
+        HTTP.get(`api/categories/` + this.category.id + `/articles/`)
+            .then(response => {
+                this.news = response.data
+                this.isFetching = false
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    },
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input-style {
+    margin-left: 10%;
+    margin-right: 10%;
+}
+</style>
+  
